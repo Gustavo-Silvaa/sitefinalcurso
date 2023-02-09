ee958b2708192f92f048afd5179a8853df0030d1
Gustavo-Silvaa <gustavosilvadasilva60@gmail.com>
2023-02-09 01:52:01 -0300

    manutencao abas inicio e sobre resposivas

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,17 +4,17 @@ import "../Css/Home.css";
 function Home() {
   return (
     <main>
-      <div id="video">
-        <iframe
-          // width="500"
-          // height="315"
-          src="https://www.youtube.com/embed/Hl3v5tQRw1Y"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-      </div>
+      <iframe
+        id="video"
+        // width="500"
+        // height="315"
+        src="https://www.youtube.com/embed/Hl3v5tQRw1Y"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+      {/* </> */}
     </main>
   );
 }
